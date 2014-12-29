229256802f26096abe8135a3487aa49bd1c7aa86
Sandile Keswa <me@sandile.io>
2014-12-29 04:27:09 -0500

    added a share button

---
+++ js/post.js
@@ -0,0 +1,14 @@
+$(function() {
+    var shareButtonConfig = {
+        description: (window.POST_TITLE ? ('"' + window.POST_TITLE + '" via @skeswa') : undefined) || 'A Cool Post on Sandile\'s Blog'
+    };
+    // Make the share button happen
+    new Share('#share-button', shareButtonConfig);
+    // Show the share button
+    setTimeout(function() {
+        $('#share-button').removeClass('concealed');
+    }, 1000);
+    setTimeout(function() {
+        $('#share-button').addClass('visible');
+    }, 1200);
+});
